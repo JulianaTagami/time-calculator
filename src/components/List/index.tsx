5058b5f363806bc5ccd792b6fb239746462d4730
JulianaTagami <jutagamiss@gmail.com>
2022-07-18 17:33:55 +0100

    input contador

---
+++ src/components/List/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Lista() {
+    return (
+        <aside>
+            <h2> Estudos do Dia </h2>
+            <ul>
+                <li>
+                    <h3>
+                        React
+                    </h3>
+
+                    <span>
+                        02:00:00
+                    </span>
+                </li>
+                <li>
+                    <h3>
+                        JavaScript
+                    </h3>
+
+                    <span>
+                        01:30:00
+                    </span>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista;

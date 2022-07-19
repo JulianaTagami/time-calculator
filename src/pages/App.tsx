7825930885039f1204d0d270a04fe0aa25382ef2
JulianaTagami <jutagamiss@gmail.com>
2022-07-19 12:21:59 +0100

    Import Style SCSS

---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Forms from '../components/Forms';
+import List from '../components/List';
+import './style.scss';
+
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Forms/>
+      <List/>
+    </div>
+  );
+}
+
+export default App;
